58954e942f0fb83e1d25cbc77333211d58748beb
fegize <114521428+fegize@users.noreply.github.com>
2023-07-11 21:33:41 -0400

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "server" {
+    source = "https://github.com/fegize/etech-team5-base-iac/tree/main/modules/ec2"
+    num = 5
+    region = "us-east-2"
+    ami = ""
+}
 
